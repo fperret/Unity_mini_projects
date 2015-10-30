8222e6dad7de65d0dd6b86271677b778975d8183
fperret <ptidinono@gmail.com>
2015-10-30 18:50:57 +0100

    version finale

--- Tetrix_2D/Assets/Scritps/Audio_manager.cs
+++ Tetrix_2D/Assets/Scritps/Audio_manager.cs
@@ -10,19 +10,41 @@ public class Audio_manager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        this.current_music = 0;
+        this.current_music = -1;
+        this.next_track();
 	}
 	
+    private void next_track()
+    {
+        this.audio_source.Stop();
+
+        this.current_music++;
+        if (this.current_music == 2)
+            this.audio_source.volume = 0.05f;
+        else
+            this.audio_source.volume = 0.1f;
+        if (this.current_music > this.musics.Length - 1)
+            this.current_music = 0;
+
+        this.audio_source.clip = this.musics[this.current_music];
+        this.audio_source.Play();
+        Invoke("next_track", this.musics[this.current_music].length);
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-	    if (!this.audio_source.isPlaying)
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            this.audio_source.clip = this.musics[this.current_music];
-            this.audio_source.Play();
-            this.current_music++;
+            if (this.audio_source.isPlaying)
+            {
+                this.audio_source.Stop();
+                CancelInvoke("next_track");
+            }
+            else
+            {
+                this.next_track();
+            }
         }
-        if (this.current_music > this.musics.Length - 1)
-            this.current_music = 0;
 	}
 }
