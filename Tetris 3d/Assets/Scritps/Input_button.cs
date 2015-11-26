982008bace5fe9d95a2ba3b709a26b49dea844a1
fperret <ptidinono@gmail.com>
2015-11-26 19:58:38 +0100

    input modification script done but not implemented

--- Tetris 3d/Assets/Scritps/Input_button.cs
+++ Tetris 3d/Assets/Scritps/Input_button.cs
@@ -1,19 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
+
+// /!\ It is important to give the right name to the correct gameobjects
 
 public class Input_button : MonoBehaviour {
 
     private bool is_on = false;
     private Button  button;
-    Color normal;
-    Color pressed;
-
+    private Color normal;
+    private Color pressed;
+    private Dictionary<string, System.Action<KeyCode> > setter_dispatch = new Dictionary<string, System.Action<KeyCode> >();
+    
     void Awake ()
     {
         this.button = this.GetComponent<Button>();
         this.normal = this.button.colors.normalColor;
         this.pressed = this.button.colors.pressedColor;
+    }
+
+    void Start ()
+    {
+        this.setter_dispatch["Move left"] = (value) => Inputs_manager.instance.set_move_left_key(value);
+        this.setter_dispatch["Move right"] = (value) => Inputs_manager.instance.set_move_right_key(value);
+        this.setter_dispatch["Rotate"] = (value) => Inputs_manager.instance.set_rotate_key(value);
+        this.setter_dispatch["Mute"] = (value) => Inputs_manager.instance.set_mute_key(value);
+        this.setter_dispatch["Drop"] = (value) => Inputs_manager.instance.set_drop_key(value);
+        this.setter_dispatch["Fall"] = (value) => Inputs_manager.instance.set_fall_key(value);
+        this.setter_dispatch["Store"] = (value) => Inputs_manager.instance.set_store_key(value);
     }
 
     public void button_clicked ()
@@ -30,13 +45,20 @@ public class Input_button : MonoBehaviour {
     }
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if (this.is_on)
+        {
+            // Tests GetKeyDown for all possible KeyCode
+            foreach(KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    this.setter_dispatch[this.name](key);
+                    this.button_clicked();
+                    break;
+                }
+            }
+        }
 	}
 }
--- Tetris 3d/Assets/Scritps/Inputs_manager.cs
+++ Tetris 3d/Assets/Scritps/Inputs_manager.cs
@@ -3,16 +3,53 @@ using System.Collections;
 
 public class Inputs_manager : MonoBehaviour {
 
-    public KeyCode left_key;
-    public KeyCode right_key;
+    public static Inputs_manager instance;
+    public KeyCode move_left_key;
+    public KeyCode move_right_key;
     public KeyCode rotate_key;
     public KeyCode mute_key;
     public KeyCode drop_key;
     public KeyCode fall_key;
+    public KeyCode store_key;
     
-    public void set_left_key (KeyCode value)
+    public void set_move_left_key (KeyCode value)
     {
-        this.left_key = value;
+        this.move_left_key = value;
+    }
+
+    public void set_move_right_key (KeyCode value)
+    {
+        this.move_right_key = value;
+    }
+
+    public void set_rotate_key (KeyCode value)
+    {
+        this.rotate_key = value;
+    }
+
+    public void set_mute_key (KeyCode value)
+    {
+        this.mute_key = value;
+    }
+
+    public void set_drop_key (KeyCode value)
+    {
+        this.drop_key = value;
+    }
+
+    public void set_fall_key (KeyCode value)
+    {
+        this.fall_key = value;
+    }
+
+    public void set_store_key (KeyCode value)
+    {
+        this.store_key = value;
+    }
+
+    void Awake()
+    {
+        instance = this;
     }
 
 	// Use this for initialization
