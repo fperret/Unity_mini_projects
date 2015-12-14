3015fec63f60e83f65b635ffcf0b349dcfa494b6
fperret <ptidinono@gmail.com>
2015-12-14 19:26:31 +0100

    camera position 2d + lighting + inputprefs save

--- Tetris 3d/Assets/Scritps/Constants.cs
+++ Tetris 3d/Assets/Scritps/Constants.cs
@@ -8,14 +8,4 @@ public class Constants : MonoBehaviour
     public const int BACK = 2;
     public const int RIGHT = 3;
     public const int UP = 4;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ Tetris 3d/Assets/Scritps/PlayerPrefs_manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefs_manager : MonoBehaviour {
+
+    public static PlayerPrefs_manager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void save_input_prefs()
+    {
+        PlayerPrefs.SetString("move_left_key", Inputs_manager.instance.move_left_key.ToString());
+        PlayerPrefs.SetString("move_right_key", Inputs_manager.instance.move_right_key.ToString());
+        PlayerPrefs.SetString("rotate_key", Inputs_manager.instance.rotate_key.ToString());
+        PlayerPrefs.SetString("mute_key", Inputs_manager.instance.mute_key.ToString());
+        PlayerPrefs.SetString("drop_key", Inputs_manager.instance.drop_key.ToString());
+        PlayerPrefs.SetString("fall_key", Inputs_manager.instance.fall_key.ToString());
+        PlayerPrefs.SetString("store_key", Inputs_manager.instance.store_key.ToString());
+    }
+}
